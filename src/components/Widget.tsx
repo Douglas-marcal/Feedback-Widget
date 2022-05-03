4daa9ab865dc95e6407bc5ef97b2970a228405d3
Douglas Marcal <douglasymarcal@gmail.com>
2022-05-03 19:20:51 -0300

    dev: Add toggle visibility on click button

--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -1,9 +1,22 @@
+import { useState } from 'react';
 import { ChatTeardropDots } from 'phosphor-react';
 
 export function Widget() {
+  const [isWidgetOpen, setIsWidgetOpen] = useState(false);
+
+  function toggleWidgetVisibility() {
+    setIsWidgetOpen(!isWidgetOpen);
+  }
+
   return (
     <div className="absolute bottom-5 right-5">
-      <button type="button" className="bg-brand-500 rounded-full h-12 px-3 text-white flex items-center group">
+      { isWidgetOpen && <p>Hello, world!</p> }
+
+      <button
+        onClick={toggleWidgetVisibility}
+        type="button"
+        className="bg-brand-500 rounded-full h-12 px-3 text-white flex items-center group"
+      >
         <ChatTeardropDots className="h-6 w-6 mx-auto" />
         <span className="max-w-0 overflow-hidden transition-all duration-500 ease-linear group-hover:max-w-xs">
           <span className="pl-2" />
