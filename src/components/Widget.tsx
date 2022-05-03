e05e6c96bfde105fc48100d4de78ee7f08d23e8c
Douglas Marcal <douglasymarcal@gmail.com>
2022-05-03 12:33:22 -0300

    dev: Create initial style to `Widget`

--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -1,5 +1,9 @@
+import { ChatTeardropDots } from 'phosphor-react';
+
 export function Widget() {
   return (
-    <h1>Widget</h1>
+    <button type="button" className="bg-violet-500 rounded-full h-12 w-12">
+      <ChatTeardropDots className="h-6 w-6 mx-auto" />
+    </button>
   );
 }
