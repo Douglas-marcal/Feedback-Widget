486ba4ea6631ecab1ad800816cffe498b0619f5f
Douglas Marcal <douglasymarcal@gmail.com>
2022-05-03 13:53:08 -0300

    feat: Create layout `Trigger Button`

--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -3,8 +3,12 @@ import { ChatTeardropDots } from 'phosphor-react';
 export function Widget() {
   return (
     <div className="absolute bottom-5 right-5">
-      <button type="button" className="bg-brand-500 rounded-full h-12 w-12 text-white">
+      <button type="button" className="bg-brand-500 rounded-full h-12 px-3 text-white flex items-center group">
         <ChatTeardropDots className="h-6 w-6 mx-auto" />
+        <span className="max-w-0 overflow-hidden transition-all duration-500 ease-linear group-hover:max-w-xs">
+          <span className="pl-2" />
+          Feedback
+        </span>
       </button>
     </div>
   );
