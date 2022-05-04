ab45cef395da52754d6a087f165dd44ec93f3cad
Douglas Marcal <douglasymarcal@gmail.com>
2022-05-04 18:49:35 -0300

    dev: Add style to textarea

--- src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -19,7 +19,7 @@ export function FeedbackContentStep(props: FeedbackContentStepProps) {
           className=" absolute top-5 left-5 text-zinc-400 hover:text-zinc-100"
           onClick={setFeedbackTypeToInitialState}
         >
-          <ArrowLeft weight="bold" className="w-4 h-4" />
+          <ArrowLeft weight="bold" className=" w-4 h-4" />
         </button>
         <span className="text-xl leading-6 flex gap-2 items-center">
           <img
@@ -32,7 +32,12 @@ export function FeedbackContentStep(props: FeedbackContentStepProps) {
         <CloseButton />
       </header>
 
-      <div className="flex py-8 gap-2 w-full" />
+      <form className="my-4 w-full">
+        <textarea
+          className="min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 focus:outline-none resize-none"
+          placeholder="Conte com detalhes o que está acontecendo..."
+        />
+      </form>
     </>
   );
 }
