352090bca9e8eb3feea3678bc10e279e80db4534
Douglas Marcal <douglasymarcal@gmail.com>
2022-05-04 15:18:00 -0300

    feat: Add source and text alternative to feedback types

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,11 +1,27 @@
+import bugImageUrl from '../images/bug.svg';
+import ideaImageUrl from '../images/idea.svg';
+import thoughtImageUrl from '../images/thought.svg';
+
 export const feedbackTypes = {
   BUG: {
     title: 'Problema',
+    image: {
+      source: bugImageUrl,
+      alt: 'Lagarta roxa',
+    },
   },
   IDEA: {
     title: 'Ideia',
+    image: {
+      source: ideaImageUrl,
+      alt: 'Lâmpada com cor amarela e bocal cinza',
+    },
   },
   OTHER: {
     title: 'Outro',
+    image: {
+      source: thoughtImageUrl,
+      alt: 'Nuvem de pensamento azul',
+    },
   },
 };
