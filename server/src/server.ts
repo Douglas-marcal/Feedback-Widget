d4e98e4113a3aa4eabb868a17eaa9549822b65b5
Douglas Marcal <douglasymarcal@gmail.com>
2022-05-16 13:18:25 -0300

    dev: Create route to add a new feedback

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,26 @@
 import express from 'express';
+import nodemailer from 'nodemailer';
+import { prisma } from './prisma';
 
 const app = express();
 const PORT = 3333;
+const CREATED = 201;
+
+app.use(express.json());
+
+app.post('/feedbacks', async (request, response) => {
+  const { comment, screenshot, type } = request.body;
+
+  const feedback = await prisma.feedback.create({
+    data: {
+      type,
+      comment,
+      screenshot,
+    },
+  });
+
+  response.status(CREATED).json({ data: feedback });
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
