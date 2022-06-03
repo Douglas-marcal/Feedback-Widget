28a465d12ec848978d3731fd4f45f93004828a35
Douglas Marcal <douglasymarcal@gmail.com>
2022-06-03 15:29:15 -0300

    test: Create unit test to use-cases

--- server/src/use-cases/submitFeedback-use-case.spec.ts
+++ server/src/use-cases/submitFeedback-use-case.spec.ts
@@ -1,1 +1,47 @@
-test('');
+/* eslint-disable @typescript-eslint/no-empty-function */
+import { SubmitFeedbackUseCase } from './submitFeedback-use-case';
+
+describe('Submit feedback', () => {
+  const createFeedbackSpy = jest.fn();
+  const sendMailSpy = jest.fn();
+
+  const submitFeedback = new SubmitFeedbackUseCase(
+    { create: createFeedbackSpy },
+    { sendMail: sendMailSpy },
+  );
+
+  it('should be able to submit feedback', async () => {
+    await expect(submitFeedback.execute({
+      type: 'BUG',
+      comment: 'example comment',
+      screenshot: 'data:image/png;base64,klsadjkisdajidsniosda',
+    })).resolves.not.toThrow();
+
+    expect(createFeedbackSpy).toHaveBeenCalled();
+    expect(sendMailSpy).toHaveBeenCalled();
+  });
+
+  it('should not be able to submit feedback without type', async () => {
+    await expect(submitFeedback.execute({
+      type: '',
+      comment: 'example comment',
+      screenshot: 'data:image/png;base64,klsadjkisdajidsniosda',
+    })).rejects.toThrowError('Type is required');
+  });
+
+  it('should not be able to submit feedback without comment', async () => {
+    await expect(submitFeedback.execute({
+      type: 'BUG',
+      comment: '',
+      screenshot: 'data:image/png;base64,klsadjkisdajidsniosda',
+    })).rejects.toThrowError('Comment is required');
+  });
+
+  it('should not be able to submit feedback without comment', async () => {
+    await expect(submitFeedback.execute({
+      type: 'BUG',
+      comment: 'bugged',
+      screenshot: 'test.png',
+    })).rejects.toThrowError('Invalid screenshot format');
+  });
+});
