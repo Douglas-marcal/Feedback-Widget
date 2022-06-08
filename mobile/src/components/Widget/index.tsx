75a67c75b6d084fbd8a0f065867ab7060e059fe2
Douglas Marcal <douglasymarcal@gmail.com>
2022-06-08 13:45:52 -0300

    dev: Add style to screen success

--- mobile/src/components/Widget/index.tsx
+++ mobile/src/components/Widget/index.tsx
@@ -3,8 +3,10 @@ import { ChatTeardropDots } from 'phosphor-react-native';
 import { TouchableOpacity} from 'react-native';
 import BottomSheet from '@gorhom/bottom-sheet';
 import { gestureHandlerRootHOC } from 'react-native-gesture-handler';
+
 import { Options } from '../Options';
 import { Form } from '../Form';
+import { Success } from '../Success';
 
 import { styles } from './styles';
 import { theme } from '../../theme';
