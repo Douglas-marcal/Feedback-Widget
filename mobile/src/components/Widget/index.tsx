4809568388c4b3e8da8d0afe6091e015f3417ae6
Douglas Marcal <douglasymarcal@gmail.com>
2022-06-08 12:41:55 -0300

    dev: Add style to textarea

--- mobile/src/components/Widget/index.tsx
+++ mobile/src/components/Widget/index.tsx
@@ -4,9 +4,13 @@ import { TouchableOpacity} from 'react-native';
 import BottomSheet from '@gorhom/bottom-sheet';
 import { gestureHandlerRootHOC } from 'react-native-gesture-handler';
 import { Options } from '../Options';
+import { Form } from '../Form';
 
 import { styles } from './styles';
 import { theme } from '../../theme';
+import { feedbackTypes } from '../../utils/feedbackTypes';
+
+export type FeedbackType = keyof typeof feedbackTypes;
 
 function Widget() {
   const bottomSheetRef = useRef<BottomSheet>(null);
